3d474f60a1e9d8355281968d1f3064c92858bf96
Kíria Cássia de Oliveira <kiriacsoliveira@gmail.com>
2024-05-31 06:52:58 -0300

    init create api node

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Kiria',
+    },
+    {
+        id: 2,
+        name: 'Marco',
+    },
+    {
+        id: 3,
+        name: 'Pedro',
+    }
+]
